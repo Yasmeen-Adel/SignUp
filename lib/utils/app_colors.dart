59aac4a2a036517065e19a40d612e2e633e50278
Yasmeen-Adel <ya1903@fayoum.edu.eg>
2025-07-28 02:24:24 +0300

    Update app_colors.dart

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
-class AppColors {
-  // static Color mainColor = Color(0xff0B3553); // أزرق غامق
+class AppColors { 
   static Color mainColor = Color.fromARGB(255, 64, 156, 255);
-  static Color secnderyColor = Color(0xff0B3553); // أحمر برتقالي دافيء
+  static Color secnderyColor = Color(0xff0B3553); 
   static Color whiteApp = Colors.white;
   static Color whiteBloc = Colors.black;
 
